e5d95be2cdb84b31b0d2ac246bbca1737f883db0
Bohdan Lutsyk <xnunrgx@gmail.com>
2024-12-09 22:19:05 +0200

    Update homework 17 to show correct state.

--- Lesson_17/test.py
+++ Lesson_17/test.py
@@ -36,14 +36,11 @@ def get_country_with_default(first_name=None):
     if first_name is not None:
         for data in country_file:
             if data[0] != '' and data[0] == first_name:
-                country = data[3]
-                break
-        if not country:
-            country = 'USA'  # Default country
+                country = 'Unknown Country'  # Default country
     return country
 
 
-# Override the original `get_address` with default value if empty.
+# # # Override the original `get_address` with default value if empty.
 def get_address_with_default():
     """Generate an address with fallback to "Unknown" if any field is empty.
 
@@ -52,8 +49,8 @@ def get_address_with_default():
     """
     full_addr = []
     addr_param = ['street', 'landmark', 'area',
-                  'city', 'pin code', 'state', 'country']
-    for i in range(5, 12):
+                  'city', 'state', 'pin code', 'country']
+    for i in range(4, 11):
         # Adjust the file path as necessary
         addr_file = csv.reader(
             open('randominfo/data.csv', 'r', encoding='utf-8'))
@@ -65,15 +62,16 @@ def get_address_with_default():
             except IndexError:
                 pass
         # Add check if all_addresses is empty, append "Unknown" if true
+        # first_name = data[0] != '' and data[0] == first_name
         if not all_addresses:
-            all_addresses.append('Unknown')
+            all_addresses.append('Unknown Country')
         full_addr.append(choice(all_addresses))
 
     full_addr = dict(zip(addr_param, full_addr))
     return full_addr
 
 
-# Override the original `get_country` and `get_address` functions.
+# # # Override the original `get_country` and `get_address` functions.
 randominfo.get_country = get_country_with_default
 randominfo.get_address = get_address_with_default
 
